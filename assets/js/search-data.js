7e29ae7ba6ae63cb1f38608b006168ef038ad58c
sreejithsiju <166488819+sreejithsiju@users.noreply.github.com>
2025-08-06 20:59:49 +0000

    Deploying to gh-pages from @ sreejithsiju/sreejithsiju.github.io@13f98217033f8b70e06b6c75f42c57bc98604e99 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,8 +9,8 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-research",
-          title: "Research",
+  },{id: "nav-publications",
+          title: "Publications",
           description: "Publications ans talks",
           section: "Navigation",
           handler: () => {
